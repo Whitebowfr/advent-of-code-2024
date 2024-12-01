6486209999ce72dc11e4533b53f87c925795d272
clementin.granier <whitebowfr@gmail.com>
2024-12-01 12:02:34 +0100

    Skipped intermediate step

--- day01.ts
+++ day01.ts
@@ -7,10 +7,8 @@ function getLists(data: string): [number[], number[]] {
 function getDistance(lists: [number[], number[]]): number {
     let dist = 0;
     while (lists[0].length != 0) {
-        let min = Math.min(...lists[0])
-        let min2 = Math.min(...lists[1])
-        lists[0].splice(lists[0].indexOf(min), 1)
-        lists[1].splice(lists[1].indexOf(min2), 1)
+        let min = lists[0].splice(lists[0].indexOf(Math.min(...lists[0])), 1)[0]
+        let min2 = lists[1].splice(lists[1].indexOf(Math.min(...lists[1])), 1)[0]
         dist += Math.abs(min - min2)
     }
     return dist
@@ -20,4 +18,4 @@ function getSimilarityScore(lists: [number[], number[]]): number {
     return lists[0].map(x => x * lists[1].map(y => Number(y == x)).reduce((a, b) => a + b)).reduce((a, b) => a + b)
 }
 
-console.log(getSimilarityScore(getLists(day1)))
+console.log(getDistance(getLists(day1)))
